80ae4378ada51079a187fd85bdcb1a4e070e1106
mannatgupta146 <mannatgupta146@gmail.com>
2025-02-03 00:38:42 +0530

    added

--- script.js
+++ script.js
@@ -1,63 +1,40 @@
-let dateContainer = document.querySelector(".date-container");
+let dayContainer = document.querySelector(".day");
+let dateContainer = document.querySelector(".date");
+let monthContainer = document.querySelector(".month");
 let hoursContainer = document.querySelector(".hours");
 let minutesContainer = document.querySelector(".minutes");
 let secondsContainer = document.querySelector(".seconds");
 
-
-const weekdays =[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wendesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+const weekdays = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 ];
 
 const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    "January", "February", "March", "April", "May", "June", "July", "August", 
+    "September", "October", "November", "December"
 ];
 
-
-function formatTime(time){
-    return time < 10 ? "0" + time : time;  //  0 : 45 : 10  -> 00 : 45 : 10 
+function formatTime(time) {
+    return time < 10 ? "0" + time : time; // 0 : 45 : 10 -> 00 : 45 : 10
 }
 
-function updateClock(){
-
-
+function updateClock() {
     const today = new Date();
-   // console.log(today);
-    let date = today.getDate();
-   // console.log(date)
     let day = weekdays[today.getDay()];
+    let date = today.getDate();
     let month = monthNames[today.getMonth()];
-
-    // console.log(day,month)
-    // console.log(today.getDay(),today.getMonth());
-
+    
     let hours = formatTime(today.getHours());
     let minutes = formatTime(today.getMinutes());
     let seconds = formatTime(today.getSeconds());
+    
+    dayContainer.textContent = day;
+    dateContainer.textContent = date;
+    monthContainer.textContent = month;
 
-
-     dateContainer.innerHTML = ` <p>${day}</p><p><span>${date}</span></p><p>${month}</p>`;   
-
-     hoursContainer.textContent = hours + ":";
-     minutesContainer.textContent = minutes + ":";
-     secondsContainer.textContent = seconds;
-
+    hoursContainer.textContent = hours + ":";
+    minutesContainer.textContent = minutes + ":";
+    secondsContainer.textContent = seconds;
 }
 
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
